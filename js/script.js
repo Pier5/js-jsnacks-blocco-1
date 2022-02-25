762e08ba31b6009b3d38cb29e89ee51ee6e23836
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-02-25 16:10:27 +0100

    -primi 5 snacks-

--- js/script.js
+++ js/script.js
@@ -51,20 +51,37 @@
 // Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
 // no alla festa.
 
-let invited = ['John', 'Geralt', 'Mia', 'Ciri', 'Triss', 'Yennefer'];
-let output = document.querySelector('.output');
-let nome = prompt('Inserisci il tuo nome');
-let found = false;
+// let invited = ['John', 'Geralt', 'Mia', 'Ciri', 'Triss', 'Yennefer'];
+// let output = document.querySelector('.output');
+// let nome = prompt('Inserisci il tuo nome');
+// let found = false;
 
-for (let i = 0; i < invited.length; i++) {
-    if (invited[i].toLowerCase() == nome.toLowerCase()) {
-        found = true;
+// for (let i = 0; i < invited.length; i++) {
+//     if (invited[i].toLowerCase() == nome.toLowerCase()) {
+//         found = true;
+//     } 
+//     if (found == true) {
+//         output.innerHTML = 'Invito alla festa confermato';
+//     } else {
+//         output.innerHTML = 'Invito alla festa non confermato';
+//     }
+// }
+
+
+// Snack 5
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+let arr = [];
+let output = document.querySelector('.output');
+
+for (i = 0; i < 6; i++) {
+    let num = parseInt(prompt('Inserisci numero'));
+    if (num % 2 == 1) {
+        arr.push(num);
+        output.innerHTML = arr;  
     } 
-    if (found == true) {
-        output.innerHTML = 'Invito alla festa confermato';
-    } else {
-        output.innerHTML = 'Invito alla festa non confermato';
-    }
 }
 
 
