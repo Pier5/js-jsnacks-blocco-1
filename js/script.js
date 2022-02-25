4912dcfa9cd5df4614be6a99833b69d5424a9d85
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-02-25 15:16:35 +0100

    primi 3 snacks

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// Snack 1
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+// const num1 = parseInt(prompt('Inserisci primo numero'));
+// const num2 = parseInt(prompt('Inserisci secondo numero'));
+
+// if (num1 > num2) {
+//     alert(num1);
+// } else if (num1 < num2) {
+//     alert(num2);
+// } else {
+//     alert('sono uguali')
+// }
+
+
+// Snack 2
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// const par1 = (prompt('Inserisci prima parola'));
+// const par2 = (prompt('Inserisci seconda parola'));
+
+// if (par1.length > par2.length) {
+//     alert(`${par1} ${par2}`);
+// } else if (par1.length < par2.length) {
+//     alert(`${par2} ${par1}`);
+// } else {
+//     alert(`${par1} ${par2}`);
+// }
+
+
+
+// Snack 3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// let somma = 0;
+
+// for (i = 0; i < 10; i++) {
+//     let num = parseInt(prompt('Inserisci numero'));
+//     somma = somma + num;
+// }
+
+// alert(`Il risultato è ${somma}`);
+
+
+
+// Snack 4
+// In un array sono contenuti i nomi degli invitati alla festa del grande
+// Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
+// no alla festa.
+
+
+
+
+
