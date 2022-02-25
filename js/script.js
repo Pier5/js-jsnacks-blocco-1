422830113ee4c79e99a4de9ec5e5281a58258e0e
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-02-25 15:52:26 +0100

    -primi 4 snacks-

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
-// const par1 = (prompt('Inserisci prima parola'));
-// const par2 = (prompt('Inserisci seconda parola'));
+// const par1 = prompt('Inserisci prima parola');
+// const par2 = prompt('Inserisci seconda parola');
 
 // if (par1.length > par2.length) {
 //     alert(`${par1} ${par2}`);
@@ -35,14 +35,14 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-// let somma = 0;
+//  let somma = 0;
 
-// for (i = 0; i < 10; i++) {
-//     let num = parseInt(prompt('Inserisci numero'));
-//     somma = somma + num;
-// }
+//  for (let i = 0; i < 10; i++) {
+//      let num = parseInt(prompt('Inserisci numero'));
+//      somma = somma + num;
+//  }
 
-// alert(`Il risultato è ${somma}`);
+//  alert(`Il risultato è ${somma}`);
 
 
 
@@ -50,6 +50,22 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande
 // Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
 // no alla festa.
+
+let invited = ['John', 'Geralt', 'Mia', 'Ciri', 'Triss', 'Yennefer'];
+let output = document.querySelector('.output');
+let nome = prompt('Inserisci il tuo nome');
+let found = false;
+
+for (let i = 0; i < invited.length; i++) {
+    if (invited[i].toLowerCase() == nome.toLowerCase()) {
+        found = true;
+    } 
+    if (found == true) {
+        output.innerHTML = 'Invito alla festa confermato';
+    } else {
+        output.innerHTML = 'Invito alla festa non confermato';
+    }
+}
 
 
 
